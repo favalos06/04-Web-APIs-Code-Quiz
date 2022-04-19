96eb437015432fea42c3d1c7365e949eafef1caa
Roman Torres <rtorres@envivent.com>
2022-04-18 18:36:01 -0700

    set up

---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+  {
+    title: "Title",
+    choices: ["this", "that", "them", "idk"],
+    answer: "this"
+  },
+  {
+    title: "Title2",
+    choices: ["this", "that", "them", "idk"],
+    answer: "that"
+  },
+  {
+    title: "Title3",
+    choices: ["this", "that", "", "idk"],
+    answer: "them"
+  },
+  {
+    title: "Title4",
+    choices: ["this", "that", "them", "idk"],
+    answer: "idk"
+  }
+];
